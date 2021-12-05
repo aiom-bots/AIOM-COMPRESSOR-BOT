93a969e04f7d487ba3e280c475e647b4b7ca2a89
Percy <percy@railway.app>
2021-12-05 16:34:50 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## 𝕍𝕚𝕕𝕖𝕠 ℂ𝕆𝕄ℙℝ𝔼𝕊𝕊𝕆ℝ 𝔹𝕆𝕋  
+
+## ᴍᴜʟᴛɪғᴜɴᴄᴛɪᴏɴ ǫᴜᴀʟɪᴛʏ ᴄᴏᴍᴘʀᴇssᴏʀ  
+
+### A Telegram Video CompressorBot  
+
+- it compress videos with negligible Quality change.
+- u can generate sample Compressed videos nd screenshots too.
+- u can set custom video name nd Thumbnail.
+- u can get logs videos to a channel too.
+- Coz of its Quality encode It takes little time to Compress.
+- For now i set it for maximum 5 Processes at a time.
+- Its Running Without Db so Block /ban /Broadcast Feature is currently Not available.
+
+### Deploy To Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+### Deploy To Railway
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fajvadntr%2FAIOM-VID-COM-BOT&envs=API_HASH%2CAPP_ID%2CBOT_TOKEN%2CLOG_CHANNEL%2COWNER_ID&API_HASHDesc=You+api+hash%2C+from+my.telegram.org+or+%40apiScrapperRoBot.&APP_IDDesc=You+api+id%2C+from+my.telegram.org+or+%40apiScrapperRoBot.&BOT_TOKENDesc=Make+a+bot+from+%40BotFather%2C+and+enter+it%27s+api+token+here.&LOG_CHANNELDesc=Create+a+private+group%2Fchannel.+Add+%40missrose_bot+and+your+bot+%2B+make+admin.+Do+%2Fid.+Paste+that+here&OWNER_IDDesc=Get+ur+Id+Of+Telegram+nd+Paste+Here.)
---
+++ helper/stuff.py
@@ -0,0 +1,129 @@
+#    This file is part of the CompressorBot distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+#    License can be found in < https://github.com/1Danish-00/CompressorBot/blob/main/License> .
+
+from .worker import *
+
+
+async def up(event):
+    if not event.is_private:
+        return
+    stt = dt.now()
+    ed = dt.now()
+    v = ts(int((ed - uptime).seconds) * 1000)
+    ms = (ed - stt).microseconds / 1000
+    p = f"🌋Pɪɴɢ = {ms}ms"
+    await event.reply(v + "\n" + p)
+
+
+async def start(event):
+    ok = await event.client(GetFullUserRequest(event.sender_id))
+    await event.reply(
+        f"***Hi `{ok.user.first_name}`\nThis is A CompressorBot Which Can Encode Videos.\nReduce Size of Videos With Negligible Quality Change\nU can Generate Samples/Screenshots too.\n\n A Bot By : @AIOM_BOTS**",
+        buttons=[
+            [Button.inline("💬 HELP", data="ihelp")],
+            [
+                Button.url("🗣️ CHANNEL", url="t.me/AIOM_BOTS"),
+                Button.url("👥 GROUP", url="t.me/AIOM_BOTS_GROUP"),
+            ],
+        ],
+    )
+
+
+async def help(event):
+    await event.reply(
+        "**A Quality CompressorBot\n\nThis Bot Compress Videos With Negligible Quality Change.\nGenerate Sample Compressed Video\nScreenshots Too\nEasy to Use\n Due to Quality Settings Bot Takes Time To Compress.\nSo Be Patience Nd Send Videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Get Options\n - @AIOM_BOTS -**",
+     )
+
+
+async def ihelp(event):
+    await event.edit(
+        "**A Quality CompressorBot\n\nThis Bot Compress Videos With Negligible Quality Change.\nGenerate Sample Compressed Video\nScreenshots Too\nEasy to Use\n Due to Quality Settings Bot Takes Time To Compress.\nSo Be Patience Nd Send Videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Get Options\n - @AIOM_BOTS -**",
+        buttons=[Button.inline("⬅️ BACK", data="beck")],
+    )
+
+
+async def beck(event):
+    ok = await event.client(GetFullUserRequest(event.sender_id))
+    await event.edit(
+        f"***Hi `{ok.user.first_name}`\nThis is A CompressorBot Which Can Encode Videos.\nReduce Size of Videos With Negligible Quality Change\nU can Generate Samples/Screenshots too.\n\n A Bot By : @AIOM_BOTS**",
+        buttons=[
+            [Button.inline("💬 HELP", data="ihelp")],
+            [
+                Button.url("🗣️ CHANNEL", url="t.me/AIOM_BOTS"),
+                Button.url("👥 GROUP", url="t.me/AIOM_BOTS_GROUP"),
+            ],
+        ],
+    )
+
+
+async def sencc(e):
+    key = e.pattern_match.group(1).decode("UTF-8")
+    await e.edit(
+        "Choose Mode",
+        buttons=[
+            [
+                Button.inline("▪️ Default Compress", data=f"encc{key}"),
+                Button.inline("▫️ Custom Compress", data=f"ccom{key}"),
+            ],
+            [Button.inline("⬅️ Back", data=f"back{key}")],
+        ],
+    )
+
+
+async def back(e):
+    key = e.pattern_match.group(1).decode("UTF-8")
+    await e.edit(
+        "**What To Do**",
+        buttons=[
+            [
+                Button.inline("◽️ GENERATE SAMPLE", data=f"gsmpl{key}"),
+                Button.inline("◾️ SCREENSHOTS", data=f"sshot{key}"),
+            ],
+            [Button.inline("🔺 COMPRESS", data=f"sencc{key}")],
+        ],
+    )
+
+
+async def ccom(e):
+    await e.edit("Send Ur Custom Name For That File")
+    wah = e.pattern_match.group(1).decode("UTF-8")
+    wh = decode(wah)
+    out, dl, thum, dtime = wh.split(";")
+    chat = e.sender_id
+    async with e.client.conversation(chat) as cv:
+        reply = cv.wait_event(events.NewMessage(from_users=chat))
+        repl = await reply
+        if "." in repl.text:
+            q = repl.text.split(".")[-1]
+            g = repl.text.replace(q, "mkv")
+        else:
+            g = repl.text + ".mkv"
+        outt = f"encode/{chat}/{g}"
+        x = await repl.reply(
+            f"Custom File Name : {g}\n\nSend Thumbnail Picture For it."
+        )
+        replyy = cv.wait_event(events.NewMessage(from_users=chat))
+        rep = await replyy
+        if rep.media:
+            tb = await e.client.download_media(rep.media, f"thumb/{chat}.jpg")
+        elif rep.text and not (rep.text).startswith("/"):
+            url = rep.text
+            os.system(f"wget {url}")
+            tb = url.replace("https://telegra.ph/file/", "")
+        else:
+            tb = thum
+        omk = await rep.reply(f"Thumbnail {tb} Setted Successfully")
+        hehe = f"{outt};{dl};{tb};{dtime}"
+        key = code(hehe)
+        await customenc(omk, key)
